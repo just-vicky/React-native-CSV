6f5a3c21feb00108ea3616a56ea9d9bdc0dc2407
Vikram <vikraman@auroville.org.in>
2024-03-07 15:51:36 +0530

    export Csv offline

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-import { View, Button, Text, FlatList } from 'react-native';
+import { View, Button, Text, FlatList, Platform, Alert } from 'react-native';
 import * as DocumentPicker from 'expo-document-picker';
 import Papa from 'papaparse';
+import * as FileSystem from 'expo-file-system';
+
+
 
 const App = () => {
   const [fileUri, setFileUri] = useState(null);
@@ -43,12 +46,48 @@ const App = () => {
       return null;
     }
   };
-  
 
+
+
+  const exportCsv = async () => {
+    if (fileUri) {
+      await exportCsvFile(fileUri);
+    } else {
+      console.error('No file selected');
+    }
+  };
+
+  const exportCsvFile = async (fileUri, fileName = 'File.csv') => {
+    try {
+      const csvContent = await FileSystem.readAsStringAsync(fileUri, { encoding: FileSystem.EncodingType.UTF8 });
+      
+      const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+      if (!permissions.granted) {
+        return;
+      }
+  
+      try {
+        await FileSystem.StorageAccessFramework.createFileAsync(permissions.directoryUri, fileName, 'text/csv')
+          .then(async (uri) => {
+            await FileSystem.writeAsStringAsync(uri, csvContent, { encoding: FileSystem.EncodingType.UTF8 });
+            alert('CSV file exported successfully');
+          })
+          .catch((e) => {
+            console.error('Error saving CSV file:', e);
+          });
+      } catch (e) {
+        console.error('Error creating CSV file:', e);
+      }
+  
+    } catch (err) {
+      console.error('Error reading file:', err);
+    }
+  };
+  
+  
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Button title="Pick a file" onPress={pickDocument} />
-      {/* {fileUri && <Text>Selected file: {fileUri}</Text>} */}
       {csvData.length > 0 && (
         <FlatList
           data={csvData}
@@ -62,6 +101,7 @@ const App = () => {
           )}
         />
       )}
+      <Button title="Pick a file" onPress={exportCsv} />
     </View>
   );
 };
